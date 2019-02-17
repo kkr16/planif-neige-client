64727f0dbcffafc9a607b50d676c55c1dfe86d84
kkr16 <21132051+kkr16@users.noreply.github.com>
2019-02-16 22:40:23 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="planif-neige-client-kkr16",
-    version="0.0.1",
+    name="planif-neige-client",
+    version="0.0.2",
     author="Karim Roukoz",
     author_email="roukoz@gmail.com",
     description="A python client for Montreal's Planif-Neige snow removal API",
