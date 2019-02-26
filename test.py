db24f5b6ec24c8f3258658589ca797d4ba043546
Karim Roukoz <karim.roukoz@ericsson.com>
2019-02-26 11:54:54 -0500

    Added basic tests using the city's staging API

---
+++ test.py
@@ -0,0 +1,41 @@
+from planif_neige_client import planif_neige_client
+import sys
+import sqlite3
+from os import remove
+from datetime import datetime, timedelta
+
+RC = 0
+DB_PATH = 'planifneige.db'
+URL_SIM = ('https://servicesenligne2.ville.montreal.qc.ca'
+           '/api/infoneige/sim/InfoneigeWebService?wsdl')
+DATE = ( datetime.now() - timedelta(
+  days=90)).replace(microsecond=0).isoformat()
+
+API_KEY = sys.argv[1]
+
+pn_client = planif_neige_client.PlanifNeigeClient(API_KEY, DB_PATH, URL_SIM)
+
+api_time_start = datetime.now()
+api_rc = pn_client.get_planification_for_date(DATE)
+api_time_end = datetime.now()
+
+print('API request ended with rc=' + str(api_rc) + ', and took ' + str((api_time_end - api_time_start).total_seconds()) + ' seconds to complete.')
+
+conn = sqlite3.connect(DB_PATH, check_same_thread=False)
+cursor = conn.cursor()
+cursor.execute('''SELECT COUNT(*) FROM planifications''')
+record_count = cursor.fetchone()[0]
+conn.close()
+
+print(record_count)
+if record_count == 0:
+  RC = 1
+
+street_planification = pn_client.get_planification_for_street(13811012)
+print(street_planification)
+if (pn_client.get_planification_for_street(13811012) is None):
+  RC = 1
+
+remove(DB_PATH)
+
+quit(RC)
