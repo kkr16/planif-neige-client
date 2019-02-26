0bd0e1f0e2e719b74e042f6afe4795507fd7b632
kkr16 <roukoz@gmail.com>
2019-02-26 09:30:22 -0500

    Fixed liting errors and renamed module to snake case

--- planif_neige_client/planif_neige_client.py
+++ planif_neige_client/planif_neige_client.py
@@ -1,13 +1,16 @@
-import zeep
-import sqlite3
+"""Python library for Montreal's snow planning APIs"""
 import datetime
 import logging
+import sqlite3
+
+import zeep
 
 URL = ('https://servicesenligne2.ville.montreal.qc.ca/'
        'api/infoneige/InfoneigeWebService?WSDL')
 
 
-class PlanifNeigeClient:
+class PlanifNeigeClient(object):
+    """Client class for the PlanifNeige API."""
     def __init__(self, token, database_path):
         self.wsdl = URL
         self.client = zeep.Client(wsdl=self.wsdl)
@@ -17,14 +20,14 @@ class PlanifNeigeClient:
         self.conn = sqlite3.connect(
             self.database_path,
             check_same_thread=False)
-        c = self.conn.cursor()
+        cursor = self.conn.cursor()
 
-        c.execute('''CREATE TABLE IF NOT EXISTS planifications (munid int,
+        cursor.execute('''CREATE TABLE IF NOT EXISTS planifications (munid int,
         coteRueId int PRIMARY KEY, etatDeneig int, dateDebutPlanif datetime,
         dateFinPlanif datetime, dateDebutReplanif datetime,
         dateFinReplanif datetime, dateMaj datetime)''')
 
-        c.execute('''CREATE TABLE IF NOT EXISTS meta (key text PRIMARY KEY,
+        cursor.execute('''CREATE TABLE IF NOT EXISTS meta (key text PRIMARY KEY,
         value text)''')
 
         self.conn.commit()
@@ -32,10 +35,10 @@ class PlanifNeigeClient:
     def get_date_last_updated(self):
         """Read from DB the date the API was last called. If date does not
         exist, assume API was never called return date 60 days ago"""
-        c = self.conn.cursor()
-        c.execute('''SELECT value from meta WHERE key="dateUpdated"''')
+        cursor = self.conn.cursor()
+        cursor.execute('''SELECT value from meta WHERE key="dateUpdated"''')
         try:
-            date_last_updated = c.fetchone()[0]
+            date_last_updated = cursor.fetchone()[0]
         except TypeError:
             date_last_updated = (
                 datetime.datetime.now() - datetime.timedelta(
@@ -44,20 +47,19 @@ class PlanifNeigeClient:
 
     def get_planification_for_street(self, street_side_id):
         """Return from DB the snow removal operations for the street"""
-        c = self.conn.cursor()
-        c.execute(
+        cursor = self.conn.cursor()
+        cursor.execute(
             'SELECT * FROM planifications WHERE coteRueId=?', [street_side_id])
-        return c.fetchone()
+        return cursor.fetchone()
 
     def get_planification_for_date(self, date=False):
         """Get from API the latest planification data for all streets since
         a specified date. If date not provided, get date from DB. """
-        c = self.conn.cursor()
+        cursor = self.conn.cursor()
         if date is False:  # if date is not specified, get date from DB
             date = self.get_date_last_updated()
-        if datetime.datetime.strptime(
-            date, '%Y-%m-%dT%H:%M:%S') < \
-                datetime.datetime.now() - datetime.timedelta(minutes=5):
+        if (datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S')
+                < datetime.datetime.now() - datetime.timedelta(minutes=5)):
             request = {'fromDate': date, 'tokenString': self.token}
             response = self.client.service.GetPlanificationsForDate(request)
             logging.error(response)
@@ -66,7 +68,7 @@ class PlanifNeigeClient:
                 data = zeep.helpers.serialize_object(
                     response)['planifications']['planification']
                 for item in data:
-                    c.execute('''INSERT OR REPLACE INTO planifications
+                    cursor.execute('''INSERT OR REPLACE INTO planifications
                     VALUES (?,?,?,?,?,?,?,?)''', (
                         item['munid'],
                         item['coteRueId'],
@@ -77,7 +79,7 @@ class PlanifNeigeClient:
                         item['dateFinReplanif'],
                         item['dateMaj']
                     ))
-                c.execute('INSERT OR REPLACE INTO meta VALUES (?,?)', (
+                cursor.execute('INSERT OR REPLACE INTO meta VALUES (?,?)', (
                     "dateUpdated",
                     (datetime.datetime.now() - datetime.timedelta(
                         minutes=1)).replace(microsecond=0).isoformat()))
